1cb23543913c8d8456403e8d2f2f5861a84c7c9f
Rohit <rc.zestgeek@gmail.com>
2022-07-01 19:55:30 +0530

    dfsdfsfd'

--- public/electron.js
+++ public/electron.js
@@ -2,7 +2,6 @@ const path = require("path");
 const fs = require("fs");
 const fsExtra = require('fs-extra')
 const Storage = require('electron-store');
-const store = new Storage
 var CaptureSSinterval = "";
 var CaptureTimeout = "";
 var keyboard = 0;
@@ -12,23 +11,38 @@ const {
   BrowserWindow,
   ipcMain,
   desktopCapturer,
+  globalShortcut
 } = require("electron");
 const isDev = require("electron-is-dev");
 const { uIOhook } = require("uiohook-napi");
+
+const schema = {
+  defaultKeyCombination: {
+    type: 'string',
+    default: 'CommandOrControl+Y'
+  }
+}
+const store = new Storage({ schema })
+
 let win = null;
+let splash;
+
+
+
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 360,
     height: 600,
     maximizable: false,
+    icon: __dirname + 'Icon.icns',
     resizable: false,
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, "./preload.js"),
     },
+    show: false
   });
-
 
 
   // and load the index.html of the app.
@@ -39,6 +53,11 @@ function createWindow() {
       : `file://${path.join(__dirname, "index.html")}`
   );
 
+  win.once('ready-to-show', () => {
+    splash.destroy();
+    win.show();
+  });
+
   // Open the DevTools.
   if (isDev) {
     win.webContents.openDevTools({ mode: "detach" });
@@ -48,7 +67,16 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+
+
+  splash = new BrowserWindow({ width: 810, height: 610, transparent: true, frame: false, icon: __dirname + 'Icon.icns', alwaysOnTop: true });
+  splash.loadURL(`file://${__dirname}/splash.html`);
+
+  createWindow()
+  globalShortcut.register('CommandOrControl+R', () => { })
+  globalShortcut.register('F5', () => { })
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -65,14 +93,21 @@ app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+
+
 });
 
+
+// app.on('browser-window-blur', function () {
+
+// });
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+// getting events from src 
+
 ipcMain.on("paused", async (event, data) => {
   keyboard = 0;
   mouse = 0;
@@ -82,6 +117,17 @@ ipcMain.on("paused", async (event, data) => {
   clearTimeout(CaptureTimeout);
 
 });
+
+ipcMain.on("quiteApp", async (event, data) => {
+  win.close();
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+
+
+
 
 
 ipcMain.on("project-started", async (event, data) => {
@@ -98,6 +144,8 @@ ipcMain.on("project-started", async (event, data) => {
 });
 
 
+// getting mouse keyboard events 
+
 uIOhook.on('mousedown', (e) => {
   mouse += 1
 })
@@ -107,6 +155,8 @@ uIOhook.on('mousedown', (e) => {
 uIOhook.on('keydown', (e) => {
   keyboard += 1
 })
+
+
 
 
 captureFunction = () => {
@@ -119,17 +169,18 @@ captureFunction = () => {
     })
     .then((sources) => {
       sources.forEach(async (source, index) => {
-        if(source.name=='Screen 1' || source.name == 'Entire Screen'){
+        if (source.name == 'Screen 1' || source.name == 'Entire Screen') {
           captureImg = source.thumbnail.toPNG();
         }
-       else if(source.name == 'Screen 2'){
+        else if (source.name == 'Screen 2') {
           captureImg2 = source.thumbnail.toPNG();
-          
+
         }
-        
-       setTimeout(()=> { fs.writeFile(
-            path.resolve(__dirname, `./images/${source.name == "Entire Screen" ? "screenshot-1.png" : source.name == "Screen 1" ?  "screenshot-1.png" :  "screenshot-2.png" }`),
-            source.name=="Entire Screen"  ? captureImg : source.name == "Screen 1" ?  captureImg :  captureImg2,
+
+        setTimeout(() => {
+          fs.writeFile(
+            path.resolve(__dirname, `./images/${source.name == "Entire Screen" ? "screenshot-1.png" : source.name == "Screen 1" ? "screenshot-1.png" : "screenshot-2.png"}`),
+            source.name == "Entire Screen" ? captureImg : source.name == "Screen 1" ? captureImg : captureImg2,
             () => {
               const windowCap = new BrowserWindow({
                 maximizable: false,
@@ -141,32 +192,31 @@ captureFunction = () => {
                 autoHideMenuBar: true,
                 frame: false,
               });
-              if(source.name == "Entire Screen"){
+              if (source.name == "Entire Screen") {
                 windowCap.loadURL(`file://${path.join(__dirname, `/screenshot.html`)}`);
                 const image = source.thumbnail.toDataURL();
-                win.webContents.send('asynchronous-message', { image, keyboard, mouse});
+                win.webContents.send('asynchronous-message', { image, keyboard, mouse });
                 keyboard = 0;
-                mouse=0;
+                mouse = 0;
 
               }
-               else if(source.name == "Screen 1" || source.name == "Screen 2"){
+              else if (source.name == "Screen 1" || source.name == "Screen 2") {
                 windowCap.loadURL(`file://${path.join(__dirname, `/multiscreenshots.html`)}`);
                 const image = source.thumbnail.toDataURL();
-                source.name == "Screen 1" ? win.webContents.send('asynchronous-message', { image, keyboard, mouse}):
-                win.webContents.send('asynchronous-message', { image, keyboard, mouse , second_screenshot : true});
+                source.name == "Screen 1" ? win.webContents.send('asynchronous-message', { image, keyboard, mouse }) :
+                  win.webContents.send('asynchronous-message', { image, keyboard, mouse, second_screenshot: true });
 
               }
               setTimeout(() => {
                 windowCap.close();
-                fsExtra.removeSync(`${__dirname}/images/${source.name=="Entire Screen"? 'screenshot-1.png' : source.name == "Screen 1" ?  "screenshot-1.png" :  "screenshot-2.png"}`)
+                fsExtra.removeSync(`${__dirname}/images/${source.name == "Entire Screen" ? 'screenshot-1.png' : source.name == "Screen 1" ? "screenshot-1.png" : "screenshot-2.png"}`)
               }, 5000);
             }
-          )}, 20);
+          )
+        }, 20);
       })
     });
 }
 
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
-
-
