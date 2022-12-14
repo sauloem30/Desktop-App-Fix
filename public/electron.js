da75692ba58767c37dca7eb55fa1d0f740d8cb8b
Iury Abreu <iury.abreu@delage.com.br>
2022-12-14 20:16:20 -0300

    cleaning the code

--- public/electron.js
+++ public/electron.js
@@ -1,21 +1,21 @@
 const path = require("path");
 const fs = require("fs");
-const fsExtra = require('fs-extra')
-const Storage = require('electron-store');
-const axios  = require('axios');
-const moment  = require('moment');
-const electron = require('electron');
+const fsExtra = require("fs-extra");
+const Storage = require("electron-store");
+const axios = require("axios");
+const moment = require("moment");
+const electron = require("electron");
 const screenElectron = electron.screen;
-const { dialog } = require('electron')
+const { dialog } = require("electron");
 const DownloadManager = require("electron-download-manager");
-const child = require('child_process');   
-const { autoUpdater } = require('electron-updater');
+const child = require("child_process");
+const { autoUpdater } = require("electron-updater");
 
 let CaptureSSinterval = "";
 let CaptureTimeout = "";
-let CaptureMouseActivity = ""
+let CaptureMouseActivity = "";
 let keyboard = 0;
-let mouse = 0
+let mouse = 0;
 const {
   app,
   BrowserWindow,
@@ -27,34 +27,35 @@ const {
 
 const isDev = require("electron-is-dev");
 const { uIOhook } = require("uiohook-napi");
-let hasMouseActivity = false
-let hasKeyboardActivity = false
+let hasMouseActivity = false;
+let hasKeyboardActivity = false;
 
-let host  = "http://3.83.193.123"
+let host = "http://3.83.193.123";
 
 DownloadManager.register({
-  downloadFolder: app.getPath("downloads") + "/installer"
+  downloadFolder: app.getPath("downloads") + "/installer",
 });
 
 const schema = {
   defaultKeyCombination: {
-    type: 'string',
-    default: 'CommandOrControl+Y'
-  }
-}
-const store = new Storage({ schema })
+    type: "string",
+    default: "CommandOrControl+Y",
+  },
+};
+const store = new Storage({ schema });
 
 let win = null;
 let splash;
-let projectData = []
+let projectData = [];
 
+//CONFIGURE AUTOUPDATER
 autoUpdater.setFeedURL({
-  provider: 'github',
-  owner: 'Thrive-VA',
-  repo: 'Desktop-App',
-  token: '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',
-  private: true
-})
+  provider: "github",
+  owner: "Thrive-VA",
+  repo: "Desktop-App",
+  token: "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",
+  private: true,
+});
 
 function createWindow() {
   // Create the browser window.
@@ -67,24 +68,26 @@ function createWindow() {
     x: 10,
     y: 10,
     maximizable: true,
-    icon: __dirname + 'Icon.icns',
+    icon: __dirname + "Icon.icns",
     resizable: false,
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, "./preload.js"),
       backgroundThrottling: false,
     },
-    show: false
+    show: false,
   });
-  win.webContents.setBackgroundThrottling(false)
+  win.webContents.setBackgroundThrottling(false);
 
-  win.webContents
-    .executeJavaScript(`localStorage.setItem("version", "${app.getVersion()}");`)
+  win.webContents.executeJavaScript(
+    `localStorage.setItem("version", "${app.getVersion()}");`
+  );
 
-  win.webContents
-    .executeJavaScript(`document.title="ThriveVA ${app.getVersion()}";`)
+  win.webContents.executeJavaScript(
+    `document.title="ThriveVA ${app.getVersion()}";`
+  );
 
-  win.removeMenu()
+  win.removeMenu();
 
   // and load the index.html of the app.
   // win.loadFile("index.html");
@@ -94,92 +97,98 @@ function createWindow() {
       : `file://${path.join(__dirname, "index.html")}`
   );
 
-  win.once('ready-to-show', () => {
+  win.once("ready-to-show", () => {
     splash.destroy();
     win.show();
   });
 
   // Open the DevTools.
-  if (!isDev) {
-    try {
-      autoUpdater.checkForUpdates();
-    } catch (err) {
-      dialog.showErrorBox(err.message, err.stack)
-    }
-  }
-
   if (isDev) {
     win.webContents.openDevTools({ mode: "detach" });
   }
 }
 
+//CHECK UPDATES
+if (!isDev) {
+  try {
+    autoUpdater.checkForUpdates();
+  } catch (err) {
+    dialog.showErrorBox(err.message, err.stack);
+  }
+}
+
 // SHOWS A MESSAGE WHEN A UPDTE AVAILABLE
 autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
-	const dialogOpts = {
-		type: 'info',
-		buttons: ['Ok'],
-		title: 'Application Update',
-		message: process.platform === 'win32' ? releaseNotes : releaseName,
-		detail: 'A new version is being downloaded.'
-	}
-	dialog.showMessageBox(dialogOpts, (response) => {
-
-	});
-})
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Ok"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail: "A new version is being downloaded.",
+  };
+  dialog.showMessageBox(dialogOpts, (response) => {});
+});
 
 //ASKS TO USER TO RESTART THE APPLICATION WHEN THE UPDATE IS READY TO BE INSTALLED
 autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
-	const dialogOpts = {
-		type: 'info',
-		buttons: ['Restart', 'Later'],
-		title: 'Application Update',
-		message: process.platform === 'win32' ? releaseNotes : releaseName,
-		detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-	};
-	dialog.showMessageBox(dialogOpts).then((returnValue) => {
-		if (returnValue.response === 0) autoUpdater.quitAndInstall()
-	})
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Restart", "Later"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates.",
+  };
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(async() => {
-  splash = new BrowserWindow({ width: 810, height: 610, transparent: true, frame: false, icon: __dirname + 'Icon.icns', alwaysOnTop: true });
+app.whenReady().then(async () => {
+  splash = new BrowserWindow({
+    width: 810,
+    height: 610,
+    transparent: true,
+    frame: false,
+    icon: __dirname + "Icon.icns",
+    alwaysOnTop: true,
+  });
   splash.loadURL(`file://${__dirname}/splash.html`);
-  createWindow()
-  globalShortcut.register('CommandOrControl+R', () => { })
-  globalShortcut.register('F5', () => { })
+  createWindow();
+  globalShortcut.register("CommandOrControl+R", () => {});
+  globalShortcut.register("F5", () => {});
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 
-const ProcessOut = async() => {
-  const {id , projectId, userId} = projectData
-  if(id && projectId && userId) {
-    const processData = async() => {
+const ProcessOut = async () => {
+  const { id, projectId, userId } = projectData;
+  if (id && projectId && userId) {
+    const processData = async () => {
       const obj = {
         time_out: moment().utc(),
-        application_type : 'desktop-auto',
+        application_type: "desktop-auto",
         project_id: projectId,
         user_id: userId,
-        id : id,
-      }
+        id: id,
+      };
       try {
         await axios.post(`${host}/api/timelog/time_out`, obj);
+      } catch (err) {
+        console.log(err);
       }
-      catch (err) {
-        console.log(err)
-      }
-    }
-    await processData()
+    };
+    await processData();
   }
-}
+};
 
-app.on("window-all-closed", async() => {
-  if (process.platform !== "darwin") { 
+app.on("window-all-closed", async () => {
+  if (process.platform !== "darwin") {
     await ProcessOut();
     app.quit();
   }
@@ -193,23 +202,21 @@ app.on("activate", () => {
   }
 });
 
-
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// getting events from src 
+// getting events from src
 
 const handlePause = () => {
   keyboard = 0;
   mouse = 0;
-  uIOhook.stop()
+  uIOhook.stop();
 
   clearInterval(CaptureSSinterval);
   clearTimeout(CaptureTimeout);
   clearInterval(CaptureMouseActivity);
-
-}
+};
 
 ipcMain.on("paused", async (event, data) => {
   handlePause();
@@ -223,72 +230,71 @@ ipcMain.on("quiteApp", async (event, data) => {
 });
 
 ipcMain.on("project-started", async (event, data) => {
-  uIOhook.start()
+  uIOhook.start();
 
-  CaptureTimeout = setTimeout(() => captureFunction(), getRandomInt(30000, 19000))
+  CaptureTimeout = setTimeout(
+    () => captureFunction(),
+    getRandomInt(30000, 19000)
+  );
   CaptureMouseActivity = setInterval(() => {
-    if(powerMonitor.getSystemIdleTime() === 1200) {
-      win.webContents.send('auto-out');
+    if (powerMonitor.getSystemIdleTime() === 1200) {
+      win.webContents.send("auto-out");
     }
 
-    if(hasMouseActivity && hasKeyboardActivity) { 
-      mouse++
-      hasMouseActivity = false
-      hasKeyboardActivity = false
-    } else if(hasMouseActivity) {
-      mouse++
-      hasMouseActivity = false
+    if (hasMouseActivity && hasKeyboardActivity) {
+      mouse++;
+      hasMouseActivity = false;
+      hasKeyboardActivity = false;
+    } else if (hasMouseActivity) {
+      mouse++;
+      hasMouseActivity = false;
     } else if (hasKeyboardActivity) {
-      keyboard++ 
-      hasKeyboardActivity = false
+      keyboard++;
+      hasKeyboardActivity = false;
     }
-  }
-  , 1000);
-  
-  CaptureSSinterval = setInterval(
-    () => {     
-      CaptureTimeout = setTimeout(() => {
-        captureFunction()
-      }, getRandomInt(10000, 199980));
-    }
-    , 199998
-  )
+  }, 1000);
+
+  CaptureSSinterval = setInterval(() => {
+    CaptureTimeout = setTimeout(() => {
+      captureFunction();
+    }, getRandomInt(10000, 199980));
+  }, 199998);
 
   win.webContents
     .executeJavaScript('localStorage.getItem("projectData");', true)
-    .then(result => {
-      projectData = JSON.parse(result)[0]
+    .then((result) => {
+      projectData = JSON.parse(result)[0];
     });
 });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
 });
 
-// getting mouse keyboard events 
-uIOhook.on('keydown', (e) => {
-  if(!hasKeyboardActivity) {
-    hasKeyboardActivity = true
+// getting mouse keyboard events
+uIOhook.on("keydown", (e) => {
+  if (!hasKeyboardActivity) {
+    hasKeyboardActivity = true;
   }
-})
+});
 
-uIOhook.on('mousedown', (e) => {
-  if(!hasMouseActivity) {
-    hasMouseActivity = true
+uIOhook.on("mousedown", (e) => {
+  if (!hasMouseActivity) {
+    hasMouseActivity = true;
   }
-})
+});
 
-uIOhook.on('mousemove', (e) => {
-  if(!hasMouseActivity) {
-    hasMouseActivity = true
+uIOhook.on("mousemove", (e) => {
+  if (!hasMouseActivity) {
+    hasMouseActivity = true;
   }
-})
+});
 
-uIOhook.on('wheel', (e) => {
-  if(!hasMouseActivity) {
-    hasMouseActivity = true
+uIOhook.on("wheel", (e) => {
+  if (!hasMouseActivity) {
+    hasMouseActivity = true;
   }
-})
+});
 
 captureFunction = () => {
   let captureImg;
@@ -302,24 +308,35 @@ captureFunction = () => {
     })
     .then((sources) => {
       sources.forEach(async (source, index) => {
-        if (source.name == 'Screen 1' || source.name == 'Entire Screen') {
+        if (source.name == "Screen 1" || source.name == "Entire Screen") {
           captureImg = source.thumbnail.toPNG();
-        }
-        else if (source.name == 'Screen 2') {
+        } else if (source.name == "Screen 2") {
           captureImg2 = source.thumbnail.toPNG();
         } else {
           return;
         }
 
         setTimeout(() => {
-          // create directory when missing 
-          const dir = path.resolve('c:/images/screenshots');
-          if (!fs.existsSync(dir)){
+          // create directory when missing
+          const dir = path.resolve("c:/images/screenshots");
+          if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
           }
           fs.writeFile(
-            path.resolve(`c:/images/screenshots/${source.name == "Entire Screen" ? "screenshot-1.png" : source.name == "Screen 1" ? "screenshot-1.png" : "screenshot-2.png"}`),
-            source.name == "Entire Screen" ? captureImg : source.name == "Screen 1" ? captureImg : captureImg2,
+            path.resolve(
+              `c:/images/screenshots/${
+                source.name == "Entire Screen"
+                  ? "screenshot-1.png"
+                  : source.name == "Screen 1"
+                  ? "screenshot-1.png"
+                  : "screenshot-2.png"
+              }`
+            ),
+            source.name == "Entire Screen"
+              ? captureImg
+              : source.name == "Screen 1"
+              ? captureImg
+              : captureImg2,
             () => {
               const windowCap = new BrowserWindow({
                 maximizable: false,
@@ -331,33 +348,62 @@ captureFunction = () => {
                 autoHideMenuBar: true,
                 frame: false,
               });
-              
+
               if (source.name == "Entire Screen") {
-                windowCap.loadURL(`file://${path.join(__dirname, `/screenshot.html`)}`);
+                windowCap.loadURL(
+                  `file://${path.join(__dirname, `/screenshot.html`)}`
+                );
                 const image = source.thumbnail.toDataURL();
-                win.webContents.send('asynchronous-message', { image, keyboard_activities_seconds: keyboard, mouse_activities_seconds: mouse, user_id: projectData.userId });
+                win.webContents.send("asynchronous-message", {
+                  image,
+                  keyboard_activities_seconds: keyboard,
+                  mouse_activities_seconds: mouse,
+                  user_id: projectData.userId,
+                });
                 keyboard = 0;
                 mouse = 0;
-
-              }
-              else if (source.name == "Screen 1" || source.name == "Screen 2") {
-                windowCap.loadURL(`file://${path.join(__dirname, `/multiscreenshots.html`)}`);
+              } else if (
+                source.name == "Screen 1" ||
+                source.name == "Screen 2"
+              ) {
+                windowCap.loadURL(
+                  `file://${path.join(__dirname, `/multiscreenshots.html`)}`
+                );
                 const image = source.thumbnail.toDataURL();
-                source.name == "Screen 1" ? win.webContents.send('asynchronous-message', { image, keyboard_activities_seconds: keyboard, mouse_activities_seconds: mouse, user_id: projectData.userId }) :
-                  win.webContents.send('asynchronous-message', { image, keyboard_activities_seconds: keyboard, mouse_activities_seconds: mouse, second_screenshot: true, user_id: projectData.userId  });
+                source.name == "Screen 1"
+                  ? win.webContents.send("asynchronous-message", {
+                      image,
+                      keyboard_activities_seconds: keyboard,
+                      mouse_activities_seconds: mouse,
+                      user_id: projectData.userId,
+                    })
+                  : win.webContents.send("asynchronous-message", {
+                      image,
+                      keyboard_activities_seconds: keyboard,
+                      mouse_activities_seconds: mouse,
+                      second_screenshot: true,
+                      user_id: projectData.userId,
+                    });
                 keyboard = 0;
                 mouse = 0;
               }
               setTimeout(() => {
                 windowCap.close();
-                fsExtra.removeSync(`c:/images/screenshots/${source.name == "Entire Screen" ? 'screenshot-1.png' : source.name == "Screen 1" ? "screenshot-1.png" : "screenshot-2.png"}`)
+                fsExtra.removeSync(
+                  `c:/images/screenshots/${
+                    source.name == "Entire Screen"
+                      ? "screenshot-1.png"
+                      : source.name == "Screen 1"
+                      ? "screenshot-1.png"
+                      : "screenshot-2.png"
+                  }`
+                );
               }, 5000);
             }
-          )
+          );
         }, 20);
-      })
+      });
     });
-}
+};
 
-
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
