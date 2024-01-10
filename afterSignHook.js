a83df1bf6dcd7c8910e58f41c6f3e80eedd9ab11
Maneet Goyal <maneetgoyal14@gmail.com>
2024-01-10 16:05:50 +0530

    refactor: minor

--- afterSignHook.js
+++ afterSignHook.js
@@ -1,23 +1,19 @@
 require('dotenv').config();
 var electron_notarize = require('electron-notarize');
 
-module.exports = async function (params) {
-    if (process.platform !== 'darwin') {
-        return;
-    }
-
-    console.log('afterSign hook triggered', params);
-
-    try {
-        await electron_notarize.notarize({
-            appBundleId: "com.klever_timeTracker",
-            appPath: "/Users/nat/Downloads/Desktop-App/electron-build/mac-arm64/Klever Desktop App.app",
-            appleId: "natsventures@gmail.com",
-            appleIdPassword: "wyjv-ucmp-kuur-nvsj",
-            tool: 'notarytool',
-            teamId: "7D6FSY3TMG",
-        });
-    } catch (error) {
-        console.error(error);
+module.exports = async function (_params) {
+    if (process.platform === 'darwin') {
+        try {
+            await electron_notarize.notarize({
+                appBundleId: "com.klever_timeTracker",
+                appPath: "electron-build/mac/Klever Desktop App.app",
+                appleId: "natsventures@gmail.com",
+                appleIdPassword: "wyjv-ucmp-kuur-nvsj",
+                tool: 'notarytool',
+                teamId: "7D6FSY3TMG",
+            });
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
