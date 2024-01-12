423055ab24e39d232d8ba686e779adcdf62f2ca0
Maneet Goyal <maneetgoyal14@gmail.com>
2024-01-12 10:20:07 +0530

    fix: Fix notarizing

--- afterSignHook.js
+++ afterSignHook.js
@@ -4,11 +4,11 @@ var { notarize } = require('@electron/notarize');
 module.exports = async function (_params) {
     if (process.platform === 'darwin') {
         try {
-            await notarize({
+            await notarize({ // Ref: https://github.com/electron/notarize?tab=readme-ov-file#method-notarizeopts-promisevoid
                 appBundleId: "com.klever_timeTracker",
                 appPath: "electron-build/mac/Klever Desktop App.app",
                 appleId: "natsventures@gmail.com",
-                appleIdPassword: "wyjv-ucmp-kuur-nvsj",
+                appleIdPassword: "wyjv-ucmp-kuur-nvsj", // The app-specific password (not your Apple ID password).
                 tool: 'notarytool',
                 teamId: "7D6FSY3TMG",
             });
